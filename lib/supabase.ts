126f2e8c306e0997dd0789448ec1cc6386c39d87
prodev1119 <actordev1119@gmail.com>
2025-07-16 14:35:27 -0400

    update-supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,5 +2,6 @@ import { createServerComponentClient } from "@supabase/ssr"
 import { cookies } from "next/headers"
 
 export function createServerSupabaseClient() {
+  console.log("createServerSupabaseClient called. Using createServerComponentClient from @supabase/ssr.")
   return createServerComponentClient({ cookies })
 }
